cebd56f8538ef533aff1b22e4be1865e76045e82
cwissy <meahchris@gmail.com>
2021-12-19 15:15:05 +0000

    es modules updates

--- templates/js/routes/users.js
+++ templates/js/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.json({ message: "I wish we had some information to give you ☹️" });
 });
 
-module.exports = router;
+export default router;
