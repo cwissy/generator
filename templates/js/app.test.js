e12712f39e1966e446fded5eefde257d600a209c
cwissy <meahchris@gmail.com>
2021-12-22 11:21:44 +0000

    supertest used with async patern and jest checks

--- templates/js/app.test.js
+++ templates/js/app.test.js
@@ -4,11 +4,11 @@ import app from "./app.js";
 
 describe("GET /users", function () {
   test("gives us back 200, with a message", async function () {
-    const expected = {
+    const expectedBody = {
       message: "I wish we had some information to give you ☹️"
     };
-    const response = await request(app).get("/users").expect(200);
-    const actual = response.body;
-    expect(actual).toStrictEqual(expected);
+    const actual = await request(app).get("/users");
+    expect(actual.body).toStrictEqual(expectedBody);
+    expect(actual.statusCode).toBe(200);
   });
 });
