a34cb00880bc6a0970b4fa3da61a4cfb02460fe3
cwissy <meahchris@gmail.com>
2021-12-22 10:51:47 +0000

    update to await the body of the repsonse in app.test.js

--- templates/js/app.test.js
+++ templates/js/app.test.js
@@ -3,9 +3,12 @@ import request from "supertest";
 import app from "./app.js";
 
 describe("GET /users", function () {
-  test("gives us back 200", function () {
-    await request(app).get("/users").expect(200, {
+  test("gives us back 200, with a message", async function () {
+    const expected = {
       message: "I wish we had some information to give you ☹️"
-    });
+    };
+    const response = await request(app).get("/users").expect(200);
+    const actual = response.body;
+    expect(actual).toStrictEqual(expected);
   });
 });
